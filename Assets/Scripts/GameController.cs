69a8a560806be6dc5c46fd0f9e18b386852b9e89
berylstar <minsang0610@naver.com>
2023-04-14 00:49:37 +0900

    0414

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -104,7 +104,6 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             stocks[shakedIndex[i]].GetComponent<StockScript>().thistype = pickedType[i];
-            // print((shakedIndex[i], pickedType[i]));
         }
 
         bigUpStage = Random.Range(7, 14);
@@ -195,15 +194,6 @@ public class GameController : MonoBehaviour
             flag_GameOver = true;
         }
     }
-
-    // 힌트 종류
-    // 1. 이전 라운드 ??한 종목 중 하나가 ??합니다. (상승/하락/유지)
-    // 2. ??? + 200$ (종목)
-    // 3. ??개의 주가가 ??합니다. (갯수) (상승/하락/유지)   -> 하락이 많으면 전체적인 경제 침체입니다 이런식으로
-    // 4. 전문가의 의견 -> 주식 전문가 ㅁㅁ씨가 ?? 종목을 매도했다는 소식입니다. + 신뢰도 ??%
-    // 5. 초반에 특수 주식 타입에 대한 정보 한 개 주기
-
-    // 상장폐지 힌트
 
     private void RandomNews()
     {
